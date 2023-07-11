d819ce1c270f735efc797885816a8dc908a04db1
ankitkicode <aj0353342@gmail.com>
2023-07-11 13:14:37 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 16, 13, 20)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 16, 13, 20)
+          ),
         useMaterial3: true,
       ),
       home: DayOne(),
